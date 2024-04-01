d2f3421a5f64374f9893e50751abcbd5ecc7ecca
pulkitt001 <pulkitpal0001@gmail.com>
2024-04-01 23:52:53 +0530

    save

---
+++ 1.searching/SearchinRotatedSortedArray.cpp
@@ -0,0 +1,27 @@
+ int search(vector<int>& nums, int target){
+        int low=0;
+        int high= nums.size()-1;
+        while(low<=high){
+            int mid=(low +high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>=nums[low]){
+                if(nums[mid]>=target && nums[low]<=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]>=target && nums[low]<=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
